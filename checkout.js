606f1eea15dd3772c06eba31b55be39c1fb42867
Ayan Mohammed <142113917+afayan@users.noreply.github.com>
2024-03-11 20:38:31 +0530

    Add files via upload

--- checkout.js
+++ checkout.js
@@ -1,19 +1,19 @@
-const cart = [];
+// myModule.js
 
-const formbox = document.querySelector('.form-box');
-const curtain = document.querySelector('.curtain');
+export const cart = [];
 
-let name = "Geen";
-let email = "ywssf@gmail.com";
-let password;
+export const formbox = document.querySelector('.form-box');
+export const curtain = document.querySelector('.curtain');
 
-//start
+export let name = "Geen";
+export let email = "ywssf@gmail.com";
+export let password;
 
-const profInfo = document.querySelector('.profInfo');
-const cartMenu = document.querySelector('.cartMenu');
-const profileMenu = document.querySelector('.profileMenu');
+export const profInfo = document.querySelector('.profInfo');
+export const cartMenu = document.querySelector('.cartMenu');
+export const profileMenu = document.querySelector('.profileMenu');
 
-function openPage() {
+export function openPage() {
     console.log('Shit is real');
     const computedStyles = window.getComputedStyle(formbox);
     const currentTop = parseInt(computedStyles.getPropertyValue('top'));
@@ -29,7 +29,7 @@ function openPage() {
     curtain.style.top = "800px";
 }
 
-function getLoginFormInfo() {
+export function getLoginFormInfo() {
     const fullName = document.querySelector('.form-box input[placeholder="Full Name"]').value;
     const email = document.querySelector('.form-box input[placeholder="Email"]').value;
     password = document.querySelector('.form-box input[placeholder="Password"]').value;
@@ -42,7 +42,7 @@ function getLoginFormInfo() {
     nameButton.innerHTML = "Hello " + fullName;
 }
 
-function addToCart(productName, price) {
+export function addToCart(productName, price) {
     const item = {
         name: productName,
         price: price
@@ -51,7 +51,7 @@ function addToCart(productName, price) {
     updateCartDisplay();
 }
 
-function updateCartDisplay() {
+export function updateCartDisplay() {
     const cartContainer = document.getElementById('cart');
     cartContainer.innerHTML = '';
 
@@ -63,7 +63,7 @@ function updateCartDisplay() {
 }
 
 // Event listeners for "Add to Cart" buttons
-const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
+export const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
 addToCartButtons.forEach(button => {
     button.addEventListener('click', function () {
         const card = this.closest('.newcard');
@@ -73,69 +73,22 @@ addToCartButtons.forEach(button => {
     });
 });
 
-function dropCart() {
+export function dropCart() {
     cartMenu.style.top = (cartMenu.style.top === '0px') ? '-600px' : '0px';
 }
 
-function dropProfile() {
+export function dropProfile() {
     profileMenu.style.top = (profileMenu.style.top === '0px') ? '-600px' : '0px';
     setProfile();
 }
 
-function setProfile() {
+export function setProfile() {
     profInfo.innerHTML = `<p>Name: ${name}</p><p>Email: ${email}</p><p>Password: ${password}</p>`;
 }
 
-function submitForm() {
+export function submitForm() {
     name = document.getElementById('name').value;
     email = document.getElementById('email').value;
     password = document.getElementById('password').value;
     console.log(`Name: ${name}\nEmail: ${email}\nPassword: ${password}`);
-}
-
-
-function LetsFilter() {
-    const selectedOptions = {};
-
-    // Get selected performance option
-    const selectedPerformance = document.querySelector('input[name="radio"]:checked');
-    if (selectedPerformance) {
-        selectedOptions.performance = selectedPerformance.nextElementSibling.innerText;
-    }
-
-    // Get selected price option
-    const selectedPrice = document.querySelector('input[name="radio2"]:checked');
-    if (selectedPrice) {
-        selectedOptions.price = selectedPrice.nextElementSibling.innerText;
-    }
-
-    // Get selected storage option
-    const selectedStorage = document.querySelector('input[name="radio3"]:checked');
-    if (selectedStorage) {
-        selectedOptions.storage = selectedStorage.nextElementSibling.innerText;
-    }
-
-    // Get selected purpose option
-    const selectedPurpose = document.querySelector('input[name="radio4"]:checked');
-    if (selectedPurpose) {
-        selectedOptions.purpose = selectedPurpose.nextElementSibling.innerText;
-    }
-
-    // Get the keywords
-    const keywords = document.querySelector('.inputFilter').value.trim();
-    if (keywords) {
-        selectedOptions.keywords = keywords;
-    }
-
-    // Log the selected options
-    console.log(selectedOptions);
-}
-
-function seeBill(params) {
-    console.log(cart)
-}
-
-function goToFilter() {
-    console.log("lets go to filter")
-    window.location.href = "filters.html";
 }
